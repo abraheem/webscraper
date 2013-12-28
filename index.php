6ffdc510fc4c93dfed6947b2885970988f16d31c
Younes Sherlock <younesagma@gmail.com>
2013-12-28 17:59:35 +0000

    Comments are added (to explain)

--- index.php
+++ index.php
@@ -7,19 +7,20 @@ use WebScraper\Scraper;
 
 $client = new Client();
 
-// FreePatentsOnline =>
+// Scraping FreePatentsOnline : "RFID"
+
 $options = array(
-		'domain_name' => 'http://www.freepatentsonline.com',
-		'source' => 'http://www.freepatentsonline.com/result.html?',
-		'result_link' => '.listing_table a',
-		'result_content' => '.document-details-wrapper',
-		'pageParameter' => 'p',
-		'incrementPage' => 1,
-		'keywordParameter' => 'query_txt',
-		'keyword' => 'rfid',
+		'domain_name' => 'http://www.freepatentsonline.com',	// The search engine domain name
+		'source' => 'http://www.freepatentsonline.com/result.html?',	// The results' search URL (without parameters)
+		'result_link' => '.listing_table a',	// The DOM element of the patents' links in the results' search page
+		'result_content' => '.document-details-wrapper', 	// The DOM element of the content in the patent page (the content to be scraped and stored in the given path - below)
+		'pageParameter' => 'p',	// The page parameter in the results' search URL
+		'incrementPage' => 1,	// The value that increments the results' search pages
+		'keywordParameter' => 'query_txt',	// The keyword parameter in the results' search URL
+		'keyword' => 'RFID',	// The keyword to search
 				);
 
 
-$path = "/opt/lampp/htdocs/Scraper/results/";
+$path = "/opt/lampp/htdocs/Scraper/results/";	// The path of the folder all files will be downloaded in
 
-Scraper::startScraping($client, $options, $path);
+Scraper::startScraping($client, $options, $path);	// Starts the scraping
